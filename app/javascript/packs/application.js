4bd69db1623266169781330aa0ad643d985daec8
Lucas Pacheco <xekonha@gmail.com>
2021-10-28 16:27:20 -0300

    Product js and navbar adjustments

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,9 +28,10 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-import { product } from '../products/product';
+import { productQuantity } from '../products/product';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  productQuantity();
 });
--- app/javascript/products/product.js
+++ app/javascript/products/product.js
@@ -4,25 +4,30 @@ var quantity = document.querySelector(".item-quantity");
 const minimum = 1;
 const maximum = 49;
 
-minus.addEventListener("click", function () {
-  if (quantity.value <= minimum) {
-    minus.disabled = true;
-    return; // return to avoid decrementing
-  } else {
-    minus.disabled = false;
+const productQuantity = () => {
+  if (minus) {
+    minus.addEventListener("click", function () {
+      if (quantity.value <= minimum) {
+        minus.disabled = true;
+        return; // return to avoid decrementing
+      } else {
+        minus.disabled = false;
+      }
+      quantity.value--;
+      if (quantity.value <= maximum) {
+        add.disabled = false;
+      }
+    });
+    add.addEventListener("click", function () {
+      quantity.value++;
+      if (quantity.value > minimum) {
+        minus.disabled = false;
+      }
+      if (quantity.value > maximum) {
+        add.disabled = true;
+      }
+    });
   }
-  quantity.value--;
-  if (quantity.value <= maximum) {
-    add.disabled = false;
-  }
-});
+}
 
-add.addEventListener("click", function () {
-  quantity.value++;
-  if (quantity.value > minimum) {
-    minus.disabled = false;
-  }
-  if (quantity.value > maximum) {
-    add.disabled = true;
-  }
-});
+export { productQuantity }
