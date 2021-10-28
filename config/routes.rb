42d1b20de11a5616f3a4ca91c0c3fa8169d17320
Lucas Pacheco <xekonha@gmail.com>
2021-10-28 15:09:06 -0300

    category resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :products, only: %i[index show new create edit update destroy category]
+  resources :orders, only: %i[index]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
