29b73b505eef4453a49b11500582e301e486a542
Rainey Lopes <rainey.lopes@gmail.com>
2021-11-01 14:59:31 -0300

    Seed de Produtos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,61 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'pry-byebug'
 
-User.all.each { |user| user.destroy } # clean db
-5.times {
-  @user = User.new
-  @user.name = Faker::Name.name
-  #@user.cpf = Faker::CPF.numeric
-  @user.email = Faker::Internet.email
-  @user.role = 'admin'
-  @user.password = '123456'
-  @user.save!
-}
-100.times {
-  @user = User.new
-  @user.name = Faker::Name.name
-  #@user.cpf = Faker::CPF.numeric
-  @user.email = Faker::Internet.email
-  @user.role = 'buyer'
-  @user.password = '123456'
-  @user.save!
-}
+# User.all.each { |usr| usr.destroy }
+@user = User.new('name' => 'rainey', 'email' => 'rainey.lopes@gmail.com', 'role' => 'admin', \
+                 'password' => '123456')
+@user.save!
+# From <supermercadinho>/db/seeds
+# To   <supermercadinho>/assets/images/products/*.jpg|*.png
+# @all_files = Dir["./assets/images/products/*"]
+new_reg = {}
+path = "app/assets/images/products/"
+file_path = "#{path}*.*"
+files = Dir[file_path]
+files.each_with_index do |file, index|
+  file_name = file.split('/')[-1]
+  file_parts = file_name.split('-')
+  parts = []
+  file_parts.each { |part| part.is_a?(String) ? parts << part.strip.squeeze(" ").titleize : parts << part }
+  # Product::CATEGORY = %w[açougue bazar bebidas congelados frios higiene
+  # hortifruti leite-derivados limpeza matinais mercearia]
+  case parts[-4].downcase
+  when 'acougue'
+    category = 'açougue'
+  when 'leitederivado', 'leitederivados', 'leitesderivados', 'leite', 'leite derivados'
+    category = 'leite-derivados'
+  when 'matinal'
+    category = 'matinais'
+  else
+    if Product::CATEGORY.include?(parts[-4].downcase)
+      category = parts[-4].downcase
+    else
+      puts "ERRO: categoria #{parts[-4]} inexistente!!!"
+      puts "parts = #{parts}"
+      puts "file = #{file}"
+    end
+  end
 
+  unit = parts[-1].split('.')[0].downcase
+  new_reg = { 'category' => category,
+              'name' => parts[-3],
+              'description' => parts[-2],
+              'unit' => unit,
+              'price' => ((0..100).to_a.sample.to_f - 0.01).round(2),
+              'total_quantity' => (1..100).to_a.sample }
+
+  product = Product.new(new_reg)
+  product.user = user
+  ext = file_name.split(".")[1].downcase
+  product.photo.attach(io: File.open(file),
+                       filename: file_name,
+                       content_type: "image/#{ext}")
+  if product.save!
+    puts "Produto #{product.name} cadastrado com sucesso."
+  else
+    puts 'ERRO!!!'
+    break
+  end
+end
